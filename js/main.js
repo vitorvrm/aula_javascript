4b4f59ba4c8fdf91443d9276d3be7498feddf804
Vitor Ribeiro <vitorribeirovrm@outlook.com>
2021-06-30 20:22:43 -0300

    aula 1
    introdução

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "Vitor"
+var idade = 19
+var idade2 = 10
+var soma = idade + idade2
+//alert("Bem vindo " + nome + ",Sua idade é " + soma)
+
+var frase = "Japão é o melhor time do mundo"
+console.log(frase.replace("Japão", "Brasil"))
+alert(frase.replace("Japão", "Brasil"))
+alert(frase.toUpperCase())
+alert(frase.toLowerCase())
