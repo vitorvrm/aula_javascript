ddea4421e6e806006ce322a422284c30486b52d2
Vitor Ribeiro <vitorribeirovrm@outlook.com>
2021-06-30 21:11:00 -0300

    finalizando primeiro bloco

--- js/main.js
+++ js/main.js
@@ -1,11 +1,71 @@
-var nome = "Vitor"
+//introdução
+/*var nome = "Vitor"
 var idade = 19
 var idade2 = 10
 var soma = idade + idade2
-//alert("Bem vindo " + nome + ",Sua idade é " + soma)
+alert("Bem vindo " + nome + ",Sua idade é " + soma)
 
 var frase = "Japão é o melhor time do mundo"
-console.log(frase.replace("Japão", "Brasil"))
-alert(frase.replace("Japão", "Brasil"))
+console.log(frase.replace("Japão", "Brasil"))*/
+
+/*alert(frase.replace("Japão", "Brasil"))
 alert(frase.toUpperCase())
-alert(frase.toLowerCase())
+alert(frase.toLowerCase())*/
+
+//Array e Dicionário
+//var lista = ["maçã", "Pêra", "Laranja"]
+//alert(lista[1])
+/*lista.push("uva")
+console.log(lista)
+lista.pop()
+console.log(lista)*/
+//console.log(lista.reverse())
+//console.log(lista.toString())
+//console.log(lista.toString()[0])
+//console.log(lista.join(" - "))
+
+/*var fruta = {
+    nome: "maçã",
+    cor: "vermelha"
+}
+
+console.log(fruta)
+console.log(fruta.nome)*/
+
+/*var frutas = [
+    {nome: "Maçã",cor: "vermelha"},
+    {nome: "Uva", cor: "roxo"},
+    {nome: "Banana", cor: "amarelo"},
+    {nome: "Pêra", cor: "verde"}
+
+]
+alert(frutas[1].nome)*/
+
+//Condicionais, laços de repetição e Date
+
+//if else
+/*var idade = prompt("Qual a sua idade ?")
+if(idade >= 18) {
+    alert("Maior de idade, pode prosseguir")
+}else {
+    alert("menor de idade, NÃO pode acessar")
+}*/
+
+//While
+/*var contador = 0
+while(contador <= 5){
+    console.log(contador)
+    contador = contador + 1
+}*/
+
+//For
+/*var contador
+for(contador=0; contador <=5; contador++){
+    console.log(contador)
+}*/
+
+//Date
+/*var data = new Date()
+console.log(data)
+console.log(data.getMinutes())
+console.log(data.getFullYear())*/
